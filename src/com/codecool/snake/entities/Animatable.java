499424fbba50efe7b641ef0e5b1c0888e4a46f18
Svetlana.Syo <svetlana.syo@gmail.com>
2018-10-18 11:05:40 +0200

    delete last version

--- src/com/codecool/snake/entities/Animatable.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.snake.entities;
-
-// Interface for animated game entities. If a GameEntity implements this, the step() method will be called
-// 60 times per second.
-public interface Animatable {
-
-    void step();
-}
