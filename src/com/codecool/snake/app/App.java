35b0bd4993ddc1fd9766732c226453bee328adb0
Svetlana.Syo <svetlana.syo@gmail.com>
2018-10-21 13:43:09 +0200

    add branches develop, features, task01

--- src/com/codecool/snake/app/App.java
+++ src/com/codecool/snake/app/App.java
@@ -1,7 +1,15 @@
 package com.codecool.snake.app;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
-        //TODO
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
