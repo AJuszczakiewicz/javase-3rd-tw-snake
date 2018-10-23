6c442a5811dcd211d983d3aba6fc9752c805dc84
Jarosław Wałach <jaroslaw.walach@gmail.com>
2018-10-23 13:31:26 +0200

    Implement observable class for model and entity

--- src/com/codecool/snake/common/ObservableEntity.java
+++ src/com/codecool/snake/common/ObservableEntity.java
@@ -2,16 +2,20 @@ package com.codecool.snake.common;
 
 import com.codecool.snake.model.AbstractGameEntity;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ObservableEntity {
-    private List<EntityObserver> observers;
+    private List<EntityObserver> observers = new ArrayList<>();
 
     public void addObserver(EntityObserver observer) {
-        //TODO
+        if(observer != null) {
+            observers.add(observer);
+        }
     }
 
     public void notifyAboutChange(AbstractGameEntity changedEntity) {
-        //TODO
+        observers.forEach(entityObserver -> entityObserver.updateOnChange(changedEntity));
     }
 }
--- src/com/codecool/snake/common/ObservableModel.java
+++ src/com/codecool/snake/common/ObservableModel.java
@@ -2,20 +2,23 @@ package com.codecool.snake.common;
 
 import com.codecool.snake.model.AbstractGameEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ObservableModel {
-    private List<ModelObserver> observators;
+    private List<ModelObserver> observators = new ArrayList<>();
 
     public void addObserver(ModelObserver observer){
-        //TODO
+        if(observer != null) {
+            observators.add(observer);
+        }
     }
 
     public void notifyAboutSpawn(AbstractGameEntity spawnedEntity){
-        //TODO
+        observators.forEach(modelObserver -> modelObserver.updateOnSpawn(spawnedEntity));
     }
 
     public void notifyAboutDestroy(AbstractGameEntity destroyedEntity){
-        //TODO
+        observators.forEach(modelObserver -> modelObserver.updateOnDestroy(destroyedEntity));
     }
 }
