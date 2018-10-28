027f951613fcc9c73a0a4243f7e87acf177bae80
maro <maretzky85@gmail.com>
2018-10-28 10:08:21 +0100

    added comments in FrameLoop

--- src/com/codecool/snake/controller/FrameControlLoop.java
+++ src/com/codecool/snake/controller/FrameControlLoop.java
@@ -3,16 +3,18 @@ package com.codecool.snake.controller;
 import static com.codecool.snake.common.Config.FRAMERATE;
 
 public class FrameControlLoop extends Thread implements Runnable {
+
     private Controller controller;
 
     private boolean isRunning = false;
 
     private int tics = 0; //For FPS Debugging
-    private long initialTime  = System.currentTimeMillis(),
-            currentTime  = System.currentTimeMillis(),
-            startTime  = System.currentTimeMillis(),
+
+    private long initialTime  = System.currentTimeMillis(), //time for Loop Control
+            startTime  = System.currentTimeMillis(), //initial time for FPS drawing
             timeFrame = 1000/FRAMERATE, //time in milliseconds for one loop;
-            counter = 0;
+            timeCounterMs = 0, //milliseconds counter
+            currentTime  = System.currentTimeMillis();
 
     public FrameControlLoop(Controller controller) {
         this.controller = controller;
@@ -23,13 +25,13 @@ public class FrameControlLoop extends Thread implements Runnable {
         isRunning = true;
         while (isRunning){
             currentTime = System.currentTimeMillis();
-            counter += (currentTime - initialTime);
+            timeCounterMs += (currentTime - initialTime);
             initialTime = currentTime;
 
-            if (counter >= timeFrame) {
+            if (timeCounterMs >= timeFrame) {
                 controller.updateModel();
                 tics += 1;
-                counter = 0;
+                timeCounterMs = 0;
             }
 
             //if statement for FPS loging in console=========
