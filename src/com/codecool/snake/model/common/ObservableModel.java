a9e14d4a85ae1ecde351244e6bdb16087e4aa242
Jarosław Wałach <jaroslaw.walach@gmail.com>
2018-11-05 13:05:35 +0100

    Add documentation to ObservableModel class

--- src/com/codecool/snake/model/common/ObservableModel.java
+++ src/com/codecool/snake/model/common/ObservableModel.java
@@ -5,19 +5,37 @@ import com.codecool.snake.model.Entity;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Holds list of observators, can take new observators
+ */
 public class ObservableModel {
     private List<ModelObserver> observators = new ArrayList<>();
 
+    /**
+     * Add new observer to list
+     *
+     * @param observer - object with implement ModelObserver
+     */
     public void addObserver(ModelObserver observer) {
         if (observer != null) {
             observators.add(observer);
         }
     }
 
+    /**
+     * Notify observators about spawning of entity
+     *
+     * @param spawnedEntity - new created entity
+     */
     protected void notifyAboutSpawn(Entity spawnedEntity) {
         observators.forEach(modelObserver -> modelObserver.updateOnSpawn(spawnedEntity));
     }
 
+    /**
+     * Notify observators about destroying of entity
+     *
+     * @param destroyedEntity - existing entity to remove
+     */
     protected void notifyAboutDestroy(Entity destroyedEntity) {
         observators.forEach(modelObserver -> modelObserver.updateOnDestroy(destroyedEntity));
     }
