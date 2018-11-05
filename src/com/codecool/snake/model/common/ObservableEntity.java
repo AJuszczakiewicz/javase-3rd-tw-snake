28066c6276eebc8027f672ec1e9c14a2ed7a6a44
Jarosław Wałach <jaroslaw.walach@gmail.com>
2018-11-05 13:27:09 +0100

    Add documentation to ObservableEntity class

--- src/com/codecool/snake/model/common/ObservableEntity.java
+++ src/com/codecool/snake/model/common/ObservableEntity.java
@@ -5,15 +5,28 @@ import com.codecool.snake.model.Entity;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Holds list of observators, can take new observators
+ */
 public class ObservableEntity {
     private List<EntityObserver> observers = new ArrayList<>();
 
+    /**
+     * Add new observer to list
+     *
+     * @param observer - object with implement EntityObserver
+     */
     public void addObserver(EntityObserver observer) {
         if (observer != null) {
             observers.add(observer);
         }
     }
 
+    /**
+     * Notify about changing in entity
+     *
+     * @param changedEntity - entity which will change
+     */
     protected void notifyAboutChange(Entity changedEntity) {
         observers.forEach(entityObserver -> entityObserver.updateOnChange(changedEntity));
     }
