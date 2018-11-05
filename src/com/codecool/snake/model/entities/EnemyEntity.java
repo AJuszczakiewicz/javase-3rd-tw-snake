1942b6b306cfe1e47343f0fd08a73dbea8951c83
Jarosław Wałach <jaroslaw.walach@gmail.com>
2018-11-05 12:54:20 +0100

    Add documentation to EnemyEntity class

--- src/com/codecool/snake/model/entities/EnemyEntity.java
+++ src/com/codecool/snake/model/entities/EnemyEntity.java
@@ -3,7 +3,14 @@ package com.codecool.snake.model.entities;
 import com.codecool.snake.model.Entity;
 import com.codecool.snake.model.common.GameEntityType;
 
+/**
+ * Holds data about enemy entity
+ */
 public class EnemyEntity extends Entity {
+
+    /**
+     * Creates new powerup entity
+     */
     public EnemyEntity() {
         super();
         setEntityType(GameEntityType.ENEMY);
